266b773889e2537c69c22bd0ba87c9ae8afb7dc2
growork <growork@mail.ru>
2020-06-30 01:09:40 +0300

    created migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+  validates :name, presence: true, length: {maximum: 35}
+  validates :department, presence: true, length: {maximum: 255}
+  validates :service_number, presence: true, length: {maximum: 10}
+  validates :service_number, uniqueness: true
 end
