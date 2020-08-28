0aa1ab569811fc3db98ac96e83d745e372d3956a
growork <growork@mail.ru>
2020-08-28 16:38:54 +0300

    удалены лишние стили scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :days
-  resources :employees
-  resources :local_sections
   devise_for :users
 
   root 'home#index'
 
+  resources :days
+  resources :employees
+  resources :local_sections
   resources :global_sections
   resources :schedules
   resources :users, only: [:show, :edit, :update]
